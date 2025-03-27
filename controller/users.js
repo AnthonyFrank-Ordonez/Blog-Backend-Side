56e12e6c33c35a9b0f5944fbc7cf9cfaa8645eda
Anthony Frank A. Ordoñez <anthonyfrankordonez@yahoo.com>
2025-03-27 19:07:01 +0800

    Added POST and GET comment API endpoint

--- controller/users.js
+++ controller/users.js
@@ -2,12 +2,13 @@ const bcrypt = require('bcrypt')
 const usersRouter = require('express').Router()
 const User = require('../models/user')
 
-
 // GET /api/users
 usersRouter.get('/', async (request, response) => {
-  const users = await User.find({}).populate('blogs', { title: 1, author: 1, url: 1 })
+  const users = await User.find({}).populate([
+    { path: 'blogs', select: 'title author url ' },
+    { path: 'comments', select: 'description createdAt updatedAt' },
+  ])
   response.json(users)
-
 })
 
 // POST /api/users
@@ -20,7 +21,7 @@ usersRouter.post('/', async (request, response) => {
   const user = new User({
     username,
     name,
-    passwordHash
+    passwordHash,
   })
 
   const savedUser = await user.save()
--- models/user.js
+++ models/user.js
@@ -5,29 +5,35 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
     unique: true,
-    minlength: 3
+    minlength: 3,
   },
   name: String,
   passwordHash: {
     type: String,
     required: true,
-    minlength: 3
+    minlength: 3,
   },
   blogs: [
     {
       type: mongoose.Schema.Types.ObjectId,
-      ref: 'Blog'
-    }
-  ]
+      ref: 'Blog',
+    },
+  ],
+  comments: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Comment',
+    },
+  ],
 })
 
 userSchema.set('toJSON', {
   transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
+    returnedObject.id = returnedObject._id?.toString() || returnedObject.id
     delete returnedObject._id
     delete returnedObject.__v
     delete returnedObject.passwordHash
-  }
+  },
 })
 
 const User = mongoose.model('User', userSchema)
